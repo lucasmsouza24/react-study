850b7a5da77cfcebc9347ff3e94a6f41b028eade
lucasmsouza24 <lucasmesquita58@hotmail.com>
2025-04-11 00:51:04 -0300

    feat: :sparkles: `useState`

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import Watch from "./components/Watch"
+
 function App() {
-  return (
-    <h1>Hello World!</h1>
-  )
+  const [actualTime, setActualTime] = useState(new Date());
+
+  // const actualTime = new Date();
+
+  return <>
+    <article>
+      <h1>Meu primeiro componente 2</h1>
+
+      <ol>
+        <li>Componentes: O Alicerce da UI</li>
+        <li>Definindo um componente</li>
+        <li>Usando um componente</li>
+      </ol>
+
+      <Watch time={actualTime.toLocaleString()} />
+
+      <button onClick={() => setActualTime(new Date())}>Update</button>
+    </article>
+  </>
 }
 
 export default App
---
+++ src/components/Watch.jsx
@@ -0,0 +1,5 @@
+export default function Watch({time}) {
+    return <>
+        <h1>{time}</h1>
+    </>
+}
